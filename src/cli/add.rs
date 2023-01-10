b3e65951f19953f5e9b27eb582171bf52dd9914d
Alex Caballero <alex.caballero.4@gmail.com>
2023-01-10 17:14:28 +0100

    Parse & validate values in add command
    Add time crate.
    
    Parse amount arg into f64 with two decimal digits.
    
    Create custom parser for date in ISO8601 format.
    
    Set now local date as default value for date.
    
    Mark category as required.

--- src/cli/add.rs
+++ src/cli/add.rs
@@ -1,5 +1,8 @@
 use clap::{Args, Subcommand};
 use std::fmt;
+use std::num::ParseFloatError;
+use time::format_description::well_known::Iso8601;
+use time::{Date, OffsetDateTime};
 
 #[derive(Subcommand)]
 pub enum Add {
@@ -22,27 +25,43 @@ impl fmt::Display for Add {
 #[derive(Args, Debug, Clone)]
 pub struct Operation {
     /// Absolute value (without sign)
-    #[arg(short, long)]
+    #[arg(short, long, value_parser = parse_float)]
     pub amount: f64,
 
-    /// Transaction date in yyyy-mm-dd format
-    #[arg(short, long)]
-    pub date: Option<String>,
-
-    /// Payment method used (e.g., debit, cash)
-    #[arg(short, long)]
-    pub payment_method: Option<String>,
+    /// Transaction date in ISO8601 format
+    #[arg(short, long, value_parser = parse_date, default_value_t = default_date())]
+    pub date: Date,
 
     /// Main category of the operation
     #[arg(short, long)]
-    pub category: Option<String>,
+    pub category: String,
 
     /// Tags to classify into subcategories (e.g., -t bill -t internet)
     #[arg(short, long)]
     pub tags: Vec<String>,
 
+    /// Payment method used (e.g., debit, cash)
+    #[arg(short, long)]
+    pub payment_method: Option<String>,
+
     /// Description of the income/expense
     pub description: Option<String>,
+}
+
+fn parse_float(arg: &str) -> Result<f64, ParseFloatError> {
+    let value = arg.trim().parse::<f64>()?;
+
+    let value = format!("{:.2}", value).parse::<f64>()?;
+
+    Ok(value)
+}
+
+fn parse_date(arg: &str) -> Result<Date, time::error::Parse> {
+    Date::parse(arg, &Iso8601::DEFAULT)
+}
+
+fn default_date() -> Date {
+    OffsetDateTime::now_local().unwrap().date()
 }
 
 pub fn add_data(add: Add) {
